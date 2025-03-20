c54939420dc392b7b0c3b962b29f39c2f9a4f8ee
Abdallah Elbashir 231003820 <abdallahcairo277@gmail.com>
2025-03-20 23:18:59 +0200

    Uploading my Code::Blocks project

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int reverseNumber(int x, int *reversed) {
+    *reversed = 0;
+    int original = x;
+
+    while (x > 0) {
+        int digit = x % 10;
+        *reversed = (*reversed * 10) + digit;
+        x /= 10;
+    }
+
+    return original == *reversed;
+}
+
+int main() {
+    int num, reversed;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("false\n");
+        printf("Reversed: Not applicable for negative numbers\n");
+    } else {
+        int isPalin = reverseNumber(num, &reversed);
+        printf("Reversed: %d\n", reversed);
+
+        if (isPalin) {
+            printf("true\n");
+        } else {
+            printf("false\n");
+        }
+    }
+
     return 0;
 }
